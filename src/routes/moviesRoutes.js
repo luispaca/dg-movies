a426de2032bb8e827dac1b0c55d320417a288ca2
luispaca <luispaca@uca.edu.ar>
2023-10-27 01:43:18 -0300

    Resolved

--- src/routes/moviesRoutes.js
+++ src/routes/moviesRoutes.js
@@ -12,6 +12,6 @@ router.get("/movies/edit/:id",moviesController.edit);
 router.post("/movies/update/:id",moviesController.update);
 router.get("/movies/delete/:id",moviesController.delete);
 router.post("/movies/delete/:id",moviesController.destroy);
-router.post("/movies/search",moviesController.search);
+router.get("/movies/search",moviesController.search);
 
 module.exports = router;
